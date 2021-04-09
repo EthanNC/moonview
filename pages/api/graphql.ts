6dbb0e5d2f95a813d28f523462d945f17eb6bab6
EthanNC <Ethan_Cumming@student.uml.edu>
2021-04-09 13:12:21 -0400

    Update graphql.ts

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,7 @@
+import { processConfig } from "@graphql-mesh/config";
 import "@graphql-mesh/graphql"
 import "@graphql-mesh/json-schema"
-import { findAndParseConfig } from '@graphql-mesh/config';
+// import { findAndParseConfig } from '@graphql-mesh/config';
 import { getMesh } from "@graphql-mesh/runtime";
 import { ApolloServer } from "apollo-server-micro";
 import { NextApiRequest, NextApiResponse } from 'next'
@@ -9,8 +10,55 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async (req:NextApiRequest, res:NextApiResponse) => {
 
   // Literal config object replaces .meshrc.yaml for serverless
-  const parsedConfig = await findAndParseConfig()
-
+  const parsedConfig = await processConfig({
+    "sources": [
+       {
+          "name": "coingecko",
+          "handler": {
+             "jsonSchema": {
+                "baseUrl": "https://api.coingecko.com/api/v3",
+                "operations": [
+                   {
+                      "type": "Query",
+                      "field": "trending",
+                      "path": "/search/trending",
+                      "method": "GET",
+                      "responseSample": "./json-samples/trending.json"
+                   },
+                   {
+                      "type": "Query",
+                      "field": "markets",
+                      "path": "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false",
+                      "method": "GET",
+                      "responseSample": "./json-samples/markets.json"
+                   },
+                   {
+                      "type": "Query",
+                      "field": "coins",
+                      "path": "/coins",
+                      "method": "GET",
+                      "responseSample": "./json-samples/coins.json"
+                   },
+                   {
+                      "type": "Query",
+                      "field": "candlestick",
+                      "path": "/coins/{args.id}/ohlc?vs_currency={args.vs_currency}&days={args.days}",
+                      "method": "GET",
+                      "responseSample": "./json-samples/candlestick.json"
+                   },
+                   {
+                      "type": "Query",
+                      "field": "ping",
+                      "path": "/ping",
+                      "method": "GET",
+                      "responseSample": "./json-samples/ping.json"
+                   }
+                ]
+             }
+          }
+       }
+    ]});
+    
   const { schema, contextBuilder: context } = await getMesh(parsedConfig);
 
   return new ApolloServer({
