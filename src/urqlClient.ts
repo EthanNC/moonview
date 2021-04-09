b26523b4dea603953e97bff32a47830ead4e06c1
EthanNC <Ethan_Cumming@student.uml.edu>
2021-04-09 12:51:48 -0400

    Update urqlClient.ts

--- src/urqlClient.ts
+++ src/urqlClient.ts
@@ -4,7 +4,6 @@ import {
   dedupExchange,
   cacheExchange,
   fetchExchange,
-  Client,
 } from "urql";
 import { devtoolsExchange } from "@urql/devtools";
 const isServerSide = typeof window === "undefined";
