e43de42fd1ff4f88da271e9b9da6f199e5c41a7f
EthanNC <Ethan_Cumming@student.uml.edu>
2021-04-09 12:38:37 -0400

    Update urqlClient.ts

--- src/urqlClient.ts
+++ src/urqlClient.ts
@@ -7,12 +7,17 @@ import {
   Client,
 } from "urql";
 import { devtoolsExchange } from "@urql/devtools";
-import { SSRExchange } from "@urql/core/dist/types/exchanges/ssr";
-import { useMemo } from "react";
 const isServerSide = typeof window === "undefined";
 const ssrCache = ssrExchange({ isClient: !isServerSide });
+const isDev = process.env.NODE_ENV === "development";
+
+
+const uri = isDev
+    ? 'http://localhost:3000/api/graphql':
+    'https://moonview.vercel.app/api/graphql'
+
 const client = createClient({
-  url: "http://localhost:3000/api/graphql",
+  url: uri,
   exchanges: [
     devtoolsExchange,
     dedupExchange,
@@ -24,53 +29,5 @@ const client = createClient({
     return { headers: {} };
   },
 });
-
-interface ClientWithSsr {
-  client: Client;
-  ssr: SSRExchange;
-}
-
-let urqlClient: ClientWithSsr;
-
-export function createUrqlClient(initialState?: Record<string, any>) {
-  let nextClient = urqlClient;
-
-  if (!nextClient) {
-    const ssr = ssrExchange({
-      isClient: !isServerSide,
-    });
-
-    const client = createClient({
-      url: !isServerSide ? "/api/graphql" : "http://localhost:3000/api/graphql",
-      fetchOptions: {
-        credentials: "include",
-      },
-      exchanges: [dedupExchange, cacheExchange, ssr, fetchExchange],
-    });
-
-    nextClient = { ssr, client };
-  }
-
-  if (initialState) {
-    nextClient.ssr.restoreData(initialState);
-  }
-
-  // For SSG and SSR always create a new Apollo Client
-  if (isServerSide) return nextClient;
-
-  // Create the Apollo Client once in the client
-  if (!urqlClient) {
-    urqlClient = nextClient;
-  }
-
-  return nextClient;
-}
-
-export function useUrqlClient(initialState?: Record<string, any>) {
-  const { client } = useMemo(() => createUrqlClient(initialState), [
-    initialState,
-  ]);
-  return client;
-}
 
 export { client, ssrCache };
